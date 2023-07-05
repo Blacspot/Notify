981da873720bc78154e4a6a6d9359d7e89762971
blacspot <obwogemcbride73@gmail.com>
2023-07-05 10:56:04 +0300

    UI created

---
+++ lib/UI/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const Color bluishClr = Color(0xFF4e5ae8);
+const Color yellowClr = Color(0xFFFFB746);
+const Color pinkClr = Color(0xFFff4667);
+const Color white = Color(0xFFFFFFFF);
+const primaryClr = bluishClr;
+const Color darkGreyClr = Color(0xFF121212);
+Color darkHeaderClr = Color(0xFF424242);
+
+class Themes {
+  static final light = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: primaryClr,
+  );
+  static final dark = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: darkGreyClr,
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:notify/UI/home_page.dart';
+import 'package:notify/UI/theme.dart';
+import 'package:notify/services/theme_services.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeServices().theme,
+      home: const HomePage(),
+    );
+  }
+}
