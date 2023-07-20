2ddbfbc341d8046e2b59904bb5506157c2ae0090
blacspot <obwogemcbride73@gmail.com>
2023-07-20 18:05:48 +0300

    addition of UI componets

--- lib/UI/addtaskbar.dart
+++ lib/UI/addtaskbar.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:notify/UI/theme.dart';
 import 'package:notify/UI/widgets/inputfield.dart';
 
-class AddTaskPage extends StatelessWidget {
+class AddTaskPage extends StatefulWidget {
   const AddTaskPage({Key? key}) : super(key: key);
 
+  @override
+  State<AddTaskPage> createState() => _AddTaskPageState();
+}
+
+class _AddTaskPageState extends State<AddTaskPage> {
+  DateTime _selectedDate = DateTime.now();
+  String _endTime = "9:30 PM";
+  String _startTime = DateFormat('hh:mm a').format(DateTime.now()).toString();
+  int _selectedRemind = 5;
+  List<int> remindList = [
+    5,
+    10,
+    15,
+    20,
+  ];
+
+  String _selectedRepeat = "None";
+  List<String> repeatList = [
+    "None",
+    "Daily",
+    "Weekly",
+    "Monthly",
+  ];
+  int _selectedColor = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,11 +46,166 @@ class AddTaskPage extends StatelessWidget {
                   "Add Task",
                   style: headingStyle,
                 ),
-                MyInputField(title: "Title", hint: "Enter your title")
+                const MyInputField(title: "Title", hint: "Enter your title"),
+                const MyInputField(title: "Note", hint: "Enter your note "),
+                MyInputField(
+                  title: "Date",
+                  hint: DateFormat.yMd().format(_selectedDate),
+                  widget: IconButton(
+                      onPressed: () {
+                        _getDateFromUser();
+                      },
+                      icon: const Icon(
+                        Icons.calendar_today_outlined,
+                        color: Colors.grey,
+                      )),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                        child: MyInputField(
+                      title: "Start Date",
+                      hint: _startTime,
+                      widget: IconButton(
+                          onPressed: () {
+                            _getTimeFromUser(isStartTime: true);
+                          },
+                          icon: const Icon(
+                            Icons.access_time_rounded,
+                            color: Colors.grey,
+                          )),
+                    )),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Expanded(
+                        child: MyInputField(
+                      title: "End Date",
+                      hint: _endTime,
+                      widget: IconButton(
+                          onPressed: () {
+                            _getTimeFromUser(isStartTime: false);
+                          },
+                          icon: const Icon(
+                            Icons.access_time_rounded,
+                            color: Colors.grey,
+                          )),
+                    ))
+                  ],
+                ),
+                MyInputField(
+                  title: "Remind",
+                  hint: "$_selectedRemind minutes early",
+                  widget: DropdownButton(
+                    icon: Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.grey,
+                    ),
+                    iconSize: 32,
+                    elevation: 4,
+                    style: subTitleStyle,
+                    underline: Container(
+                      height: 0,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedRemind = int.parse(newValue!);
+                      });
+                    },
+                    items:
+                        remindList.map<DropdownMenuItem<String>>((int value) {
+                      return DropdownMenuItem<String>(
+                        value: value.toString(),
+                        child: Text(value.toString()),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                MyInputField(
+                  title: "Repeat",
+                  hint: "$_selectedRepeat",
+                  widget: DropdownButton(
+                    icon: Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.grey,
+                    ),
+                    iconSize: 32,
+                    elevation: 4,
+                    style: subTitleStyle,
+                    underline: Container(
+                      height: 0,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedRepeat = newValue!;
+                      });
+                    },
+                    items: repeatList
+                        .map<DropdownMenuItem<String>>((String? value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child:
+                            Text(value!, style: TextStyle(color: Colors.grey)),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Row(
+                  children: [
+                    _colorPallete(),
+                  ],
+                )
               ],
             ),
           ),
         ));
+  }
+
+  _colorPallete() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Color",
+          style: titleStyle,
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        Wrap(
+          children: List<Widget>.generate(3, (int index) {
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  _selectedColor = index;
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: CircleAvatar(
+                  radius: 14,
+                  backgroundColor: index == 0
+                      ? primaryClr
+                      : index == 1
+                          ? pinkClr
+                          : yellowClr,
+                  child: _selectedColor == index
+                      ? Icon(
+                          Icons.done,
+                          color: Colors.white,
+                          size: 16,
+                        )
+                      : Container(),
+                ),
+              ),
+            );
+          }),
+        )
+      ],
+    );
   }
 
   _appBar(BuildContext context) {
@@ -48,5 +228,47 @@ class AddTaskPage extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  _getDateFromUser() async {
+    DateTime? _pickerDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2022),
+        lastDate: DateTime(2100));
+
+    if (_pickerDate != null) {
+      setState(() {
+        _selectedDate = _pickerDate;
+      });
+    } else {
+      print("it's null or something is wrong");
+    }
+  }
+
+  _getTimeFromUser({required bool isStartTime}) async {
+    var pickedTime = await _showTimePicker();
+    String _formatedTime = pickedTime.format(context);
+    if (pickedTime == null) {
+      print("Time cancelled");
+    } else if (isStartTime == true) {
+      setState(() {
+        _startTime = _formatedTime;
+      });
+    } else if (isStartTime == false) {
+      setState(() {
+        _endTime = _formatedTime;
+      });
+    }
+  }
+
+  _showTimePicker() {
+    return showTimePicker(
+        initialEntryMode: TimePickerEntryMode.input,
+        context: context,
+        initialTime: TimeOfDay(
+          hour: int.parse(_startTime.split(":")[0]),
+          minute: int.parse(_startTime.split(":")[1].split("")[0]),
+        ));
   }
 }
